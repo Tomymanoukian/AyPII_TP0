c1935bb008d16a2040565a6fdbed773dec4e6434
InspectorDave <55307710+InspectorDave@users.noreply.github.com>
2019-09-29 05:01:20 -0300

    Agrego los archivos dft
    La función dft debería funcionar, todavía no solucioné el tema de las aproximaciones (precisión del double). Tampoco hice la idft.

---
+++ dft.cpp
@@ -0,0 +1,27 @@
+#include "dft.h"
+
+vector_t dft (vector_t vector_in)
+{
+	int k, n, largo_in=vector_in.vector_t::leng();
+	vector_t vector_out(largo_in);
+	complejo aux{};
+
+	for (k=0; k<largo_in; k++)
+	{
+		for (n=0, aux.complejo::set_re(0), aux.complejo::set_im(0); n<largo_in; n++) 
+		{
+			aux.complejo::set_re(aux.complejo::re() + (vector_in.vector_t::valor(n).complejo::re())*cos(2*M_PI*n*k/largo_in) - (vector_in.vector_t::valor(n).complejo::im())*sin(2*M_PI*n*k/largo_in));
+			aux.complejo::set_im(aux.complejo::im() + (vector_in.vector_t::valor(n).complejo::re())*sin(2*M_PI*n*k/largo_in) - (vector_in.vector_t::valor(n).complejo::im())*cos(2*M_PI*n*k/largo_in));
+		}
+		vector_out.vector_t::swap(aux,k);
+	}
+
+	return vector_out;
+}
+
+vector_t idft (vector_t vector_in)
+{
+	vector_t vector_out;
+
+	return vector_out;
+}
