cb077d1c9fc876b09c3a0675ed0ced40b0371711
InspectorDave <55307710+InspectorDave@users.noreply.github.com>
2019-09-29 15:34:10 -0300

    Agrego idft
    Agrego la funcion idft, sigue faltando solucionar el tema de las aproximaciones

--- dft.cpp
+++ dft.cpp
@@ -21,7 +21,21 @@ vector_t dft (vector_t vector_in)
 
 vector_t idft (vector_t vector_in)
 {
-	vector_t vector_out;
+	int n, k, largo_in=vector_in.vector_t::leng();
+	vector_t vector_out(largo_in);
+	complejo aux{};
+
+	for (n=0; n<largo_in; n++)
+	{
+		for (k=0, aux.complejo::set_re(0), aux.complejo::set_im(0); k<largo_in; k++)
+		{
+			aux.complejo::set_re(aux.complejo::re() + (vector_in.vector_t::valor(k).complejo::re())*cos(2*M_PI*k*n/largo_in) + (vector_in.vector_t::valor(k).complejo::im())*sin(2*M_PI*k*n/largo_in));
+			aux.complejo::set_im(aux.complejo::im() + (vector_in.vector_t::valor(k).complejo::re())*sin(2*M_PI*k*n/largo_in) + (vector_in.vector_t::valor(k).complejo::im())*cos(2*M_PI*k*n/largo_in));
+		}
+		aux.complejo::set_re(aux.complejo::re()/largo_in);
+		aux.complejo::set_im(aux.complejo::im()/largo_in);
+		vector_out.vector_t::swap(aux,n);
+	}
 
 	return vector_out;
 }
