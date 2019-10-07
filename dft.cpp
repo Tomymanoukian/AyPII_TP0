2bf3c3b9f17d1b4a5897f2370509760945b52190
Tomas Fernandez <tomymanoukian@gmail.com>
2019-10-07 00:50:02 -0300

    corrijo error de cálculo de la dft e idft

--- dft.cpp
+++ dft.cpp
@@ -2,16 +2,16 @@
 
 vector_t * dft (vector_t & vector_in)
 {
-	int k, n, largo_in=vector_in.leng();
+	int k, n, largo_in = vector_in.leng();
 	vector_t * vector_out = new vector_t(largo_in);
 	complejo aux{};
 
-	for (k=0; k<largo_in; k++)
+	for (k = 0; k < largo_in; k++)
 	{
-		for (n=0, aux.set_re(0), aux.set_im(0); n<largo_in; n++) 
+		for (n = 0, aux.set_re(0), aux.set_im(0); n < largo_in; n++) 
 		{
-			aux.set_re(aux.re() + (vector_in.valor(n).re())*cos(2*M_PI*n*k/largo_in) - (vector_in.valor(n).im())*sin(2*M_PI*n*k/largo_in));
-			aux.set_im(aux.im() + (vector_in.valor(n).re())*sin(2*M_PI*n*k/largo_in) - (vector_in.valor(n).im())*cos(2*M_PI*n*k/largo_in));
+			aux.set_re(aux.re() + (vector_in.valor(n).re())*cos(2*M_PI*n*k/largo_in) + (vector_in.valor(n).im())*sin(2*M_PI*n*k/largo_in));
+			aux.set_im(aux.im() - (vector_in.valor(n).re())*sin(2*M_PI*n*k/largo_in) + (vector_in.valor(n).im())*cos(2*M_PI*n*k/largo_in));
 		}
 		vector_out->append(aux);
 	}
@@ -21,19 +21,19 @@ vector_t * dft (vector_t & vector_in)
 
 vector_t * idft (vector_t & vector_in)
 {
-	int n, k, largo_in=vector_in.leng();
+	int n, k, largo_in = vector_in.leng();
 	vector_t * vector_out = new vector_t(largo_in);
 	complejo aux{};
 
-	for (n=0; n<largo_in; n++)
+	for (n = 0; n < largo_in; n++)
 	{
-		for (k=0, aux.set_re(0), aux.set_im(0); k<largo_in; k++)
+		for (k=0, aux.set_re(0), aux.set_im(0); k < largo_in; k++)
 		{
-			aux.set_re(aux.re() + (vector_in.valor(k).re())*cos(2*M_PI*k*n/largo_in) + (vector_in.valor(k).im())*sin(2*M_PI*k*n/largo_in));
+			aux.set_re(aux.re() + (vector_in.valor(k).re())*cos(2*M_PI*k*n/largo_in) - (vector_in.valor(k).im())*sin(2*M_PI*k*n/largo_in));
 			aux.set_im(aux.im() + (vector_in.valor(k).re())*sin(2*M_PI*k*n/largo_in) + (vector_in.valor(k).im())*cos(2*M_PI*k*n/largo_in));
 		}
-		aux.set_re(aux.re()/largo_in);
-		aux.set_im(aux.im()/largo_in);
+		aux.set_re(aux.re() / largo_in);
+		aux.set_im(aux.im() / largo_in);
 		vector_out->append(aux);
 	}
 
